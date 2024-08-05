13f3f8ed9a7511df86ad54b08ff463b1877cfb25
Bot WhatsApp HACKING <154031706+HACKING995@users.noreply.github.com>
2024-08-05 13:41:18 +0000

    test new

--- index.js
+++ index.js
@@ -416,7 +416,7 @@
                 } 
 
 
-if (_0x45f822.endsWith("@s.whatsapp.net") && _0x37a424 != _0x5d8568) {
+if (texte && auteurMessage.endsWith("@s.whatsapp.net") && _0x37a424 != _0x5d8568) {
       if (_0x55afff?.["etat"] == 'on') {
         if (_0x55afff.lien == "no url") {
           _0x540c51(_0x55afff.message);
