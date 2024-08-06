9f60d821a0d292ef709d4108c81cf0d7157336cc
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-08-06 00:44:34 +0000

    Update index.js

--- index.js
+++ index.js
@@ -248,6 +248,28 @@
                 
                 };
 
+                // Chat_bot 
+
+                if (conf.CHAT_BOT === 'oui') {
+                   const traduction = require("./framework/traduction");
+    let trdmsg = await traduction(inputMessage, { 'to': 'en' });
+                    
+fetch("https://api.brainshop.ai/get?bid=182939&key=Je69ped2ZzbfNf3g&uid=[uid]&msg=[msg][uid]&msg=" + trdmsg)
+      .then(response => response.json()) 
+      .then(data => {
+        const respmsg = data.cnt;
+        traduction(respmsg, { 'to': 'fr' })
+       .then(finalMessage => {
+          repondre(finalMessage);
+        }).catch(error => {
+          console.error("Erreur lors de la traduction en français :", error);
+        });
+      }).catch(error => {
+        console.error("Erreur lors de la requête à BrainShop :", error);
+      });
+                }
+                // fin Chat_bot
+
                 /************************ anti-delete-message */
 
                     if(ms.message.protocolMessage && ms.message.protocolMessage.type === 0 && (conf.ATD).toLocaleLowerCase() === 'oui' ) {
